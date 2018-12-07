bf20555a20af4aec0038c7665e266334a3e1184b
laislp <45685732+laislp@users.noreply.github.com>
2018-12-07 09:12:28 -0200

    Add files via upload

---
+++ myclosetbot.py
@@ -0,0 +1,90 @@
+from database import DBHelper
+import json 
+import requests
+import time
+import urllib
+
+TOKEN = "775188559:AAF4NI9aiAxtQeO7gyu41O8gHqPiwJU7VJY"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+db = DBHelper()
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def handle_updates(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        items = db.get_items(chat)
+        if text == "/done":
+            keyboard = build_keyboard(items)
+            send_message("Selecione uma peça para deletar", chat, keyboard)
+        elif text == "/start":
+            send_message("Bem vindo ao seu armário! Eu sou seu assistente de estilo. \n\n Para adicionar uma peça de roupa, descreva sua peça seguindo o modelo: \n tipo material cor. (exemplo: jaqueta couro preta) \n\n Para remover uma peça, mande /done ou descreva a peça novamente.", chat)
+        elif text.startswith("/"):
+            continue
+        elif text in items:
+            db.delete_item(text, chat)
+            items = db.get_items(chat)
+            keyboard = build_keyboard(items)
+            send_message("Selecione uma peça para deletar", chat, keyboard)
+        else:
+            db.add_item(text, chat)
+            items = db.get_items(chat)
+            message = "\n".join(items)
+            send_message(message, chat)
+
+def send_message(text, chat_id, reply_markup=None):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
+    get_url(url)
+    
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
+
+def main():
+    db.setup()
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            handle_updates(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
